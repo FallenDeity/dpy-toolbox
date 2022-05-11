9be0a08792b1b5b07374bf9132a8c239484457c4
TheWever <85771814+TheWever@users.noreply.github.com>
2022-05-11 15:58:57 +0200

    Add files via upload

--- docs/setup.py
+++ docs/setup.py
@@ -10,6 +10,6 @@ setup(name='dpy_toolbox',
       packages=['dpy_toolbox'],
       zip_safe=False,
       install_requires=[
-          'git+https://github.com/Rapptz/discord.py/tree/master#egg=discord',
+          'git+https://github.com/Rapptz/discord.py#egg=discord',
           ]
       )
