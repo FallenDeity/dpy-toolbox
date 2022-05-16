a52fdb70d365e1186b1c1f9e125e802d963280c2
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-05-16 19:27:08 +0530

    Hey asher here
     Made the example a bit more detailed

--- examples/bot.py
+++ examples/bot.py
@@ -1,11 +1,11 @@
-from dpy_toolbox import Bot
-import discord
-TOKEN = ''  # lol please dont do that this is just an example / put that in a .env
+from dpy_toolbox import Bot #pip install dpy-toolbox
+import discord #install dpy 2.0 pip install -U git+https://github.com/Rapptz/discord.py@rewrite#egg=discord.py[voice] Note: You need PyNaCl for voice support.
+TOKEN = ''  #Just for example purposes. Please put the token in an .env file and use "token=os.environ['TOKEN']".
 
-bot = Bot(command_prefix='!', intents=discord.Intents.all())
+bot = Bot(command_prefix='!', intents=discord.Intents.all()) #Enable Intents in your bot page.
 
-@bot.event
+@bot.event 
 async def on_ready():
-    print(f'Running as {bot.user}')
+    print(f'Running as {bot.user}') #prints bot name when up and running.
 
-bot.run(TOKEN)
+bot.run(TOKEN) #add your token fetched from environment.
